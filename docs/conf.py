cc71c674e044f31ff154b8cc7a8f55975e4ff2ad
arpras <avenkatraman@linkedin.com>
2015-04-07 15:20:52 -0700

    Fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,6 @@
 
 # -*- coding: utf-8 -*-
 import sys, os, datetime
-sys.path.append("/Users/avenkatr/Zopkio")
-import zopkio
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
