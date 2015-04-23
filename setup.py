f2addddc3cb9230dd474cd2de3d833851220773a
arpras <avenkatraman@linkedin.com>
2015-04-22 17:35:44 -0700

    update to latest version 0.1.18

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
   author = 'Joshua Ehrlich',
   author_email = 'jehrlich@linkedin.com,zopkio@googlegroups.com',
   url = 'http://github.com/linkedin/zopkio',
-  download_url = 'https://github.com/linkedin/zopkio/tarball/0.1.17',
+  download_url = 'https://github.com/linkedin/zopkio/tarball/0.1.18',
   license = 'Apache',
   packages = ['zopkio', 'zopkio.web_resources'],
   package_dir = { 'zopkio' : 'zopkio'},
