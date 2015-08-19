6f35f2b3aa87c0e50966dfdfcc3f67b7bb540d0a
chuongddh <chuong@pubnub.com>
2015-08-19 11:52:01 -0700

    remove log dir from description for now due to absolute path issue

--- zopkio/reporters/junit_reporter.py
+++ zopkio/reporters/junit_reporter.py
@@ -107,8 +107,8 @@ class Reporter(object):
           test_time = 0
           if test.func_end_time != None and test.func_start_time != None:
               test_time = test.func_end_time - test.func_start_time
-          report_description = test.description + '\n' + self.report_info.logs_dir
-          tc = TestCase(test.name,'',test_time, report_description, test.message)
+          # report_description = test.description + '\n' + self.report_info.logs_dir
+          tc = TestCase(test.name,'',test_time, test.description, test.message)
           if 'failed' in test.result:
               tc.add_failure_info(test.result)
           elif 'skipped' in test.result:
